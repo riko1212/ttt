e0e51ab59c76cdd6004dd66173336eecece8aad3
riko1212 <46224717+riko1212@users.noreply.github.com>
2022-12-16 09:45:05 +0000

    Deploying to gh-pages from @ riko1212/ttt@faf48d404a59eaef0a75e68b62272ba6b95280ce 🚀

---
+++ restaurant.25561265.js
@@ -0,0 +1,2 @@
+$(".slider-2pictures").slick({mobileFirst:!0,slidesToShow:2,adaptiveHeight:!0}),$(".slider-2pictures-invert").slick({mobileFirst:!0,slidesToShow:2,adaptiveHeight:!0}),$(".slider-about").slick({mobileFirst:!0,slidesToShow:2,variableWidth:!0});
+//# sourceMappingURL=restaurant.25561265.js.map
